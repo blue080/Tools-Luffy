b4bd04093411e3eb3244010268ea91b9e0ced18e
叶飞 <yefei@jizhijiafu.cn>
2016-12-29 16:19:59 +0800

    fix index bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /**
  * Created by blue on 2016/12/28.
  */
-export { default as browser} from './browser'
-export { default as Cookie} from './Cookie'
-export { default as Platform} from './Platform'
-export { default as Str} from './Str'
-export { default as Type} from './Type'
-export { default as Util} from './Util'
-export { default as Verify} from './Verify'
+export browser from './browser'
+export Cookie from './Cookie'
+export Platform from './Platform'
+export Str from './Str'
+export Type from './Type'
+export Util from './Util'
+export Verify from './Verify'
